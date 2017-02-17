a19e6ff098588afc589b355141837e00ecfe144c
wenlarry <wenlarry@icloud.com>
2017-02-17 17:52:07 +0800

    Add files via upload

---
+++ Quiz_3.R
@@ -0,0 +1,35 @@
+#Quiz 3
+
+#1 R Pkg to pass R CMD check without warnings/errors
+#
+# Ans -  An explicit softward licence
+#
+# One way to anser this Q is start with and existing R pkg and remove things 
+# by one
+#
+#Q2
+# Generic functions in a R installation, with only the defaul pkg
+#
+# Ans -  predict; show; mean
+#
+#Q3
+# Function used to obtain the function body for a S4 method function
+#
+# Ans - getMethod()
+#
+#Q4
+# Download R pkg DDPQuiz3 from the course website and examine the 
+# 'createmean' function in the R/sub-directory. What is the appropriate 
+# text to place above the 'createmean' function for Roxygen2 to create a
+# complete help file ?
+#
+# Ans:
+# #'This function calculates the mean
+# #'
+# #' @param x is a numeric vector
+# #' @return the mean of x
+# #' @export
+# #' @examples
+# #' x <- 1:10
+# #' createmean(x) 
+#
